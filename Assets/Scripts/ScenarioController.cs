0a254d2dee69e512f76ce813f393a7c619d1dc21
nkrishnaswamy <nkrishn87@gmail.com>
2020-09-21 09:31:44 -0600

    Add object spawner

--- Assets/Scripts/ScenarioController.cs
+++ Assets/Scripts/ScenarioController.cs
@@ -11,12 +11,16 @@ public class ScenarioController : MonoBehaviour
     public GameObject interactableObjects;
     public GameObject surface;
     public Transform objectTypes;
+    public int numTotalObjs;
+    public int numInteractableObjs;
 
     Camera mainCamera;
 
     Vector3 floorPosition;
 
     bool objectsInited;
+
+    VoxemeInit voxemeInit;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,7 @@ public class ScenarioController : MonoBehaviour
 
         floorPosition = new Vector3(mainCamera.transform.position.x, 0.0f, mainCamera.transform.position.z);
 
+        voxemeInit = GameObject.Find("VoxWorld").GetComponent<VoxemeInit>();
         objectsInited = false;
     }
 
@@ -40,18 +45,50 @@ public class ScenarioController : MonoBehaviour
 
     void PlaceRandomly(GameObject surface)
     {
-        // place focus objects
-        foreach (Transform t in objectTypes)
+        // place interactable objects
+        for (int i = 0; i < numInteractableObjs; i++)
         {
-            Debug.Log(t);
-            Vector3 coord = GlobalHelper.FindClearRegion(surface, t.gameObject).center;
+            Transform t = objectTypes.GetChild(RandomHelper.RandomInt(0, objectTypes.childCount - 1,
+                (int)(RandomHelper.RangeFlags.MinInclusive | RandomHelper.RangeFlags.MaxInclusive)));
+            Debug.Log(string.Format("Creating new {0}", t.name));
+
+            Vector3 coord;
+            do
+            {
+                coord = GlobalHelper.FindClearRegion(surface, t.gameObject).center;
+            } while (Vector3.Magnitude(coord - floorPosition) < interactableRadius);
+
             GameObject newObj = Instantiate(t.gameObject);
-            Debug.Log(newObj);
+            newObj.name = newObj.name.Replace("(Clone", "");
             newObj.transform.position = new Vector3(coord.x,
                 coord.y + GlobalHelper.GetObjectWorldSize(newObj.gameObject).extents.y, coord.z);
             //newObj.GetComponent<Voxeme>().targetPosition = newObj.transform.position;
 
             newObj.transform.parent = interactableObjects.transform;
+            voxemeInit.InitializeVoxemes();
+        }
+
+        // place non-interactable objects
+        for (int i = 0; i < numTotalObjs-numInteractableObjs; i++)
+        {
+            Transform t = objectTypes.GetChild(RandomHelper.RandomInt(0, objectTypes.childCount - 1,
+                (int)(RandomHelper.RangeFlags.MinInclusive | RandomHelper.RangeFlags.MaxInclusive)));
+            Debug.Log(string.Format("Creating new {0}", t.name));
+
+            Vector3 coord;
+            do
+            {
+                coord = GlobalHelper.FindClearRegion(surface, t.gameObject).center;
+            } while (Vector3.Magnitude(coord - floorPosition) > interactableRadius);
+
+            GameObject newObj = Instantiate(t.gameObject);
+            newObj.name = newObj.name.Replace("(Clone", "");
+            newObj.transform.position = new Vector3(coord.x,
+                coord.y + GlobalHelper.GetObjectWorldSize(newObj.gameObject).extents.y, coord.z);
+            //newObj.GetComponent<Voxeme>().targetPosition = newObj.transform.position;
+
+            newObj.transform.parent = interactableObjects.transform;
+            voxemeInit.InitializeVoxemes();
         }
     }
 }
