2c765a997f75d82b00ab0dc684e9d5dd3d830578
nkrishnaswamy <nkrishn87@gmail.com>
2021-04-22 13:21:50 -0600

    Do not accept action if base.StepCount == 0

--- Assets/Scripts/StackingAgent.cs
+++ Assets/Scripts/StackingAgent.cs
@@ -17,19 +17,122 @@ public class StackingAgent : Agent
     public ScenarioController scenarioController;
 
     List<Transform> interactableObjs;
-    
+
     VectorSensor sensor;
 
     float[] lastAction;
 
-    bool objectsPlaced = false;
-    bool waitingForAction = false;
-    bool episodeStarted = false;
-    bool running = false;
-    bool executingEvent = false;
-    bool resolvePhysics = false;
-    bool constructObservation = false;
-    bool endEpisode = false;
+    bool _objectsPlaced = false;
+    public bool objectsPlaced
+    {
+        get { return _objectsPlaced; }
+        set
+        {
+            if (_objectsPlaced != value)
+            {
+                OnObjectsPlacedChanged(_objectsPlaced, value);
+            }
+            _objectsPlaced = value;
+        }
+    }
+
+    bool _waitingForAction = false;
+    public bool waitingForAction
+    {
+        get { return _waitingForAction; }
+        set
+        {
+            if (_waitingForAction != value)
+            {
+                OnWaitingForActionChanged(_waitingForAction, value);
+            }
+            _waitingForAction = value;
+        }
+    }
+
+    bool _episodeStarted = false;
+    public bool episodeStarted
+    {
+        get { return _episodeStarted; }
+        set
+        {
+            if (_episodeStarted != value)
+            {
+                OnEpisodeStartedChanged(_episodeStarted, value);
+            }
+            _episodeStarted = value;
+        }
+    }
+
+    bool _running = false;
+    public bool running
+    {
+        get { return _running; }
+        set
+        {
+            if (_running != value)
+            {
+                OnRunningChanged(_running, value);
+            }
+            _running = value;
+        }
+    }
+
+    bool _executingEvent = false;
+    public bool executingEvent
+    {
+        get { return _executingEvent; }
+        set
+        {
+            if (_executingEvent != value)
+            {
+                OnExecutingEventChanged(_executingEvent, value);
+            }
+            _executingEvent = value;
+        }
+    }
+
+    bool _resolvePhysics = false;
+    public bool resolvePhysics
+    {
+        get { return _resolvePhysics; }
+        set
+        {
+            if (_resolvePhysics != value)
+            {
+                OnResolvePhysicsChanged(_resolvePhysics, value);
+            }
+            _resolvePhysics = value;
+        }
+    }
+
+    bool _constructObservation = false;
+    public bool constructObservation
+    {
+        get { return _constructObservation; }
+        set
+        {
+            if (_constructObservation != value)
+            {
+                OnConstructObservationChanged(_constructObservation, value);
+            }
+            _constructObservation = value;
+        }
+    }
+
+    bool _endEpisode = false;
+    public bool endEpisode
+    {
+        get { return _endEpisode; }
+        set
+        {
+            if (_endEpisode != value)
+            {
+                OnEndEpisodeChanged(_endEpisode, value);
+            }
+            _endEpisode = value;
+        }
+    }
 
     float observation, lastObservation;
 
@@ -60,13 +163,14 @@ public class StackingAgent : Agent
         {
             observation = ConstructObservation();
             Debug.LogFormat("StackingAgent.FixedUpdate: Observation = {0}; Reward = {1}", observation, observation - lastObservation);
-            AddReward((observation - lastObservation) > 0 ? (observation - lastObservation) : (observation - lastObservation)-1);
+            AddReward((observation - lastObservation) > 0 ? (observation - lastObservation) : (observation - lastObservation) - 1);
 
             if (observation - lastObservation != 0)
             {
                 // figure out which is the new destination object
                 List<Transform> sortedByHeight = interactableObjs.OrderByDescending(t => t.position.y).ToList();
 
+                OnDestObjChanged(destObj, sortedByHeight.First().gameObject);
                 destObj = sortedByHeight.First().gameObject;
             }
 
@@ -179,9 +283,10 @@ public class StackingAgent : Agent
         //  will actually be processed
         scenarioController.ClearEventManager();
         executingEvent = false;
-
+        
         Debug.Log("StackingAgent.OnEpisodeBegin: Beginning episode");
         observation = 1;
+        sensor.AddObservation(observation);
 
         scenarioController.PlaceRandomly(scenarioController.surface);
         PhysicsHelper.ResolveAllPhysicsDiscrepancies(false);
@@ -201,7 +306,7 @@ public class StackingAgent : Agent
             }
             else
             {
-                sensor.AddObservation(-1);
+                sensor.AddObservation(lastObservation);
             }
 
             waitingForAction = true;
@@ -210,12 +315,17 @@ public class StackingAgent : Agent
         {
             // if the episode has terminated, return the last observation
             //  (i.e., the observation at the final state)
-            sensor.AddObservation(lastObservation);
+            sensor.AddObservation(observation);
         }
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
+        if (base.StepCount == 0)
+        {
+            return;
+        }
+
         if (waitingForAction && !executingEvent)
         {
             Vector2 targetOnSurface = new Vector2(vectorAction[0], vectorAction[1]);
@@ -224,6 +334,8 @@ public class StackingAgent : Agent
             {
                 if (!vectorAction.SequenceEqual(lastAction))
                 {
+                    OnThemeObjChanged(themeObj, SelectThemeObject());
+                    //when this happens the physics resolution hasn't finished yet so the new position of the destination object hasn't updated
                     themeObj = SelectThemeObject();
 
                     if (themeObj == null)
@@ -242,6 +354,8 @@ public class StackingAgent : Agent
 
                     Bounds themeBounds = GlobalHelper.GetObjectWorldSize(themeObj);
                     Bounds destBounds = GlobalHelper.GetObjectWorldSize(destObj);
+                    Debug.Log(GlobalHelper.VectorToParsable(destBounds.center));
+                    Debug.Log(GlobalHelper.VectorToParsable(destBounds.max));
                     Vector3 targetPos = new Vector3(
                         destBounds.center.x + (destBounds.size.x * targetOnSurface.x),
                         destBounds.max.y + themeBounds.extents.y,
@@ -276,5 +390,107 @@ public class StackingAgent : Agent
     public override void Heuristic(float[] actionsOut)
     {
 
+    }
+
+    /// <summary>
+    /// Triggered when the themeObj flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of themeObj
+    //      newVal -- new or current value of themeObj
+    void OnThemeObjChanged(GameObject oldVal, GameObject newVal)
+    {
+        Debug.Log(string.Format("==================== themeObj changed ==================== {0}->{1}",
+            oldVal == null ? "NULL" : oldVal.name, newVal == null ? "NULL" : newVal.name));
+    }
+
+    /// <summary>
+    /// Triggered when the destObj flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of destObj
+    //      newVal -- new or current value of destObj
+    void OnDestObjChanged(GameObject oldVal, GameObject newVal)
+    {
+        Debug.Log(string.Format("==================== destObj changed ==================== {0}->{1}",
+            oldVal == null ? "NULL" : oldVal.name, newVal == null ? "NULL" : newVal.name));
+    }
+
+    /// <summary>
+    /// Triggered when the objectsPlaced flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of objectsPlaced
+    //      newVal -- new or current value of objectsPlaced
+    void OnObjectsPlacedChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== objectsPlaced flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the waitingForAction flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of waitingForAction
+    //      newVal -- new or current value of waitingForAction
+    void OnWaitingForActionChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== waitingForAction flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the episodeStarted flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of episodeStarted
+    //      newVal -- new or current value of episodeStarted
+    void OnEpisodeStartedChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== episodeStarted flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the running flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of running
+    //      newVal -- new or current value of running
+    void OnRunningChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== running flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the executingEvent flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of executingEvent
+    //      newVal -- new or current value of executingEvent
+    void OnExecutingEventChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== executingEvent flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the resolvePhysics flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of resolvePhysics
+    //      newVal -- new or current value of resolvePhysics
+    void OnResolvePhysicsChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== resolvePhysics flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the constructObservation flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of constructObservation
+    //      newVal -- new or current value of constructObservation
+    void OnConstructObservationChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== constructObservation flag changed ==================== {0}->{1}", oldVal, newVal));
+    }
+
+    /// <summary>
+    /// Triggered when the endEpisode flag changes
+    /// </summary>
+    // IN: oldVal -- previous value of endEpisode
+    //      newVal -- new or current value of endEpisode
+    void OnEndEpisodeChanged(bool oldVal, bool newVal)
+    {
+        Debug.Log(string.Format("==================== endEpisode flag changed ==================== {0}->{1}", oldVal, newVal));
     }
 }
