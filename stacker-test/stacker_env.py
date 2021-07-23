08fcd11e88d580f53080aae3a0dd3a8cb93837df
nkrishnaswamy <nkrishn87@gmail.com>
2021-07-22 20:00:39 -0600

    Add description to stacker_env

--- stacker-test/stacker_env.py
+++ stacker-test/stacker_env.py
@@ -6,6 +6,62 @@ from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.base_env import ActionTuple
 
 class StackerEnv(gym.Env):
+    """
+    Description:
+        A number of objects are placed in an otherwise empty environment. All
+        objects start on the floor and the goal is to place all objects into
+        a single stack by placing each in turn  on top of the current stack so
+        that the stack stays upright after the placement is complete.
+        
+    Observation:
+        Type: Discrete (4 scalar values)
+        The size of this space should be adjusted to always be greater than the
+        total number of potentially-stackable objects in the environment. The
+        number represents the height of the highest object tower constructed so
+        far, i.e., 1 is the default observation because all objects are alone on
+        the ground.
+        Num     Observation
+        0       0 objects have been successfully stacked (would only occur if the environment were empty)
+        1       1 object has been successfully stacked (default state because all objects are on the ground)
+        2       2 objects have been successfully stacked (highest structure in the environment is a 2-block stack)
+        3       3 objects have been successfully stacked
+        
+    Actions:
+        Type: Box (2 dimensions)
+        The action space represents where, as a ratio in two dimensions, the theme
+        object should be placed relative to the top surface of the destination
+        object. That is, an action of [0.0,0.0] would place the theme object at
+        the exact center of the top of the destination object, while an action of
+        [-1.0,0.0] would place it at the far left edge. The actions, when received
+        by Unity, are scaled to the size of the destination object and transformed
+        into 3D space, so only these two values need to be provided by the RL client.
+        Num     Action                          Min                     Max
+        0       X value of object placement     -1.0                    1.0
+        1       Y value of object placement     -1.0                    1.0
+        
+    Reward:
+        Reward is 1 if an action results in a stack that is one object higher and
+        stable. Reward is -1 if an action results in one object falling off the stack,
+        -2 if it results in two objects falling off the stack, etc.
+                
+    Starting State:
+        The starting observation is 1, because all objects are on the ground, therefore
+        the "highest stack" is 1 block high.
+                
+    Episode Termination:
+        All objects in the environment have been successfully stacked.
+        Episode length is greater than the "Max Step" value specified in the
+        "StackingAgent" class in the Unity Editor.
+        Solved Requirements:
+        Because the reward is contructed in terms of "did the theme object stay where
+        it was placed?" vs. "did the theme object fall down?" a successful policy
+        should learn to produce action values that are closer to [0.0,0.0] in this
+        action space. Considered solved when the average return is greater than or
+        equal to 0.75 over 100 trials, meaning a 3/4 probability of placing the theme
+        object in a stable position on the first try, with no more than one failed
+        try per episode.
+    """
+    
     def __init__(self,
         environment_filename=None):
         self._env = UnityEnvironment(environment_filename,0)
